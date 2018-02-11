8a7a23935df3160c44af7f6b69854b1caecf2d61
Dmitrii K <winston.smith.spb@gmail.com>
2018-02-12 02:22:25 +0700

    Refr

--- setup.py
+++ setup.py
@@ -18,14 +18,14 @@ setup(
         'lxml==4.1.1',
         'fake-useragent==0.1.10'
     ],
-classifiers=(
+    classifiers=(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT Licens',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-),
+    ),
 )
