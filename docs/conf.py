be8cb87753df2c432913bf58e665aac410d3007d
Dmitrii Kurlov <dmitriik@tutanota.com>
2020-08-13 18:31:21 +0700

    fix sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 from extract_emails import __version__
+autodoc_mock_imports = ['selenium', 'requests']
 
 
 # -- Project information -----------------------------------------------------
