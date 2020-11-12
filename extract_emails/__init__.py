e52dce9e41ccbb79e214757b492ff839457f81ae
chiamin <az564123@gmail.com>
2020-11-12 16:38:40 +0800

    fix: fix set bug

--- extract_emails/__init__.py
+++ extract_emails/__init__.py
@@ -1,3 +1,3 @@
 __version__ = "4.0.3"
-from .extract_emails import EmailExtractor, EmailExtractorWithFB
+from .extract_emails import EmailExtractor
 from .email import Email
--- extract_emails/html_handlers/default_html_handler.py
+++ extract_emails/html_handlers/default_html_handler.py
@@ -11,6 +11,7 @@ class DefaultHTMLHandler(HTMLHandlerInterface):
             r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
         )
         self.link_pattern = re.compile(r'<a\s+(?:[^>]*?\s+)?href=(["\'])(.*?)\1')
+        
 
     def get_emails(self, page_source: str) -> List[str]:
         """
@@ -19,7 +20,7 @@ class DefaultHTMLHandler(HTMLHandlerInterface):
         :param str page_source: HTML page source
         :return: List of emails
         """
-        return list(set(self.email_pattern.findall(page_source)))
+        return self.email_pattern.findall(page_source)
 
     def get_links(self, page_source: str) -> List[str]:
         """
@@ -31,3 +32,8 @@ class DefaultHTMLHandler(HTMLHandlerInterface):
         links = self.link_pattern.findall(page_source)
         links = [x[1] for x in links]
         return links
+
+    
+
+
+## get link from facebook
