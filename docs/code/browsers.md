f9fd9f4f82899d0300a63b1978406bd06eb60392
Dmitrii Kurlov <dmitriik@tutanota.com>
2021-09-11 01:44:01 +0700

    Refactoring ChromBrowser part 2, final

--- docs/code/browsers.md
+++ docs/code/browsers.md
@@ -1,3 +1,5 @@
 # Browsers
 
 ::: extract_emails.browsers.page_source_getter
+
+::: extract_emails.browsers.chrome_browser
--- extract_emails/browsers/chrome_browser.py
+++ extract_emails/browsers/chrome_browser.py
@@ -2,33 +2,25 @@ from os import PathLike
 from typing import Iterable
 from typing import Optional
 
+from loguru import logger
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-from extract_emails.browsers import PageSourceGetter
-
-from .browser_interface import BrowserInterface
-
-
-"""
-Chrome Browser
----------------
-Make GET requests via selenium + Google Chrome Browser
-
-:param bool headless: run browser in headless mode, default: True
-:param str executable_path: path to the executable. If the default is used it assumes the executable is in the $PATH
-
-**Example:**
-::
-
-    browser = ChromeBrowser()
-    browser.open()
-    page_source = browser.get_page_source('https://example.com')
-    browser.close()
-"""
+from extract_emails.browsers.page_source_getter import PageSourceGetter
 
 
 class ChromeBrowser(PageSourceGetter):
+    """Getting page sources with selenium and chromedriver
+
+    Examples:
+        >>> from extract_emails.browsers import ChromeBrowser
+        >>> browser = ChromeBrowser()
+        >>> browser.open()
+        >>> # do something
+        >>> browser.close()
+
+    """
+
     default_options = {
         "--disable-gpu",
         "--disable-software-rasterizer",
@@ -43,11 +35,21 @@ class ChromeBrowser(PageSourceGetter):
         executable_path: PathLike = "/usr/bin/chromedriver",
         headless_mode: bool = True,
         options: Iterable[str] = None,
-    ):
+    ) -> None:
+        """ChromeBrowser initialization
+
+        Args:
+            executable_path: path to chromedriver, use `which chromedriver` to get the path.
+                Default: /usr/bin/chromedriver
+            headless_mode: run browser with headless mode or not. Default: True
+            options: arguments for chrome.Options().
+                Default: set("--disable-gpu", "--disable-software-rasterizer", "--disable-dev-shm-usage",
+                    "--window-size=1920x1080", "--disable-setuid-sandbox", "--no-sandbox", )
+        """
         self.executable_path = executable_path
         self.headless_mode = headless_mode
         self.options = options if options is not None else self.default_options
-        self.driver = None
+        self.driver: Optional[webdriver.Chrome] = None
 
     def open(self):
         """Add arguments to chrome.Options() and run the browser"""
@@ -63,29 +65,24 @@ class ChromeBrowser(PageSourceGetter):
         )
 
     def close(self):
+        """Close the browser"""
         self.driver.close()
         self.driver.quit()
 
-    def _get(self, url: str) -> bool:
+    def get_page_source(self, url: str) -> str:
+        """Get page source text from URL
+
+        Args:
+            url: URL
+
+        Returns:
+            page source as text
+        """
         try:
             self.driver.get(url)
-            return True
-        except:
-            return False
-
-    def get_page_source(self, url: str) -> str:
-        """
-        Return page source from url
-
-        :param str url: page url
-        :return: str, HTML from page
-
-        **Example:**
-        ::
-
-            page_source = browser.get_page_source('https://example.com')
-        """
-        is_get = self.get(url)
-        if is_get:
-            return self.driver.page_source
-        return ""
+            page_source = self.driver.page_source
+        except Exception as e:
+            logger.error(f"Could not get page source from {url}")
+            return ""
+        else:
+            return page_source
