2812fbac111ffa5f318a35de0e6cf0ea6d32c080
Dmitrii Kurlov <dmitriik@tutanota.com>
2021-09-13 04:00:29 +0700

    Add more tests for ContactInfoLinkFilter

--- tests/link_filters/test_contact_link_filter.py
+++ tests/link_filters/test_contact_link_filter.py
@@ -31,3 +31,18 @@ def test_custom_candidates():
     assert "https://google.com" not in filtered_links
     assert "https://example.com/page1" not in filtered_links
     assert "https://example.com/not-default-call-us-2" in filtered_links
+
+
+def test_use_default_true():
+    link_filter = ContactInfoLinkFilter("https://example.com", ["not-call-us"])
+    filtered_links = link_filter.filter(test_urls)
+    assert len(filtered_links) == 6
+    assert "https://example.com/page" in filtered_links
+
+
+def test_use_default_false():
+    link_filter = ContactInfoLinkFilter(
+        "https://example.com", ["not-call-us"], use_default=False
+    )
+    filtered_links = link_filter.filter(test_urls)
+    assert len(filtered_links) == 0
