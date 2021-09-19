d4a94b6d7129afc8cdf09686db9a7d3dc38412f3
Dmitrii Kurlov <dmitriik@tutanota.com>
2021-09-20 01:24:08 +0700

    Add linkedin data extractor

--- docs/code/data_extractors.md
+++ docs/code/data_extractors.md
@@ -2,3 +2,4 @@
 
 ::: extract_emails.data_extractors.data_extractor
 ::: extract_emails.data_extractors.email_extractor
+::: extract_emails.data_extractors.linkedin_extractor
---
+++ tests/data_extractors/test_linkedin_extractor.py
@@ -0,0 +1,16 @@
+from extract_emails.data_extractors import LinkedinExtractor
+
+
+STRING = """
+The good thing about your LinkedIn URL is that you can have your customized URL. By default, LinkedIn will provide you a profile URL that is alphanumeric and is a combination of your name and numbers.
+For example, it will look something similar to this: https://www.linkedin.com/in/venjamin-brant-73381ujy3u
+The good thing about your LinkedIn URL is that you can have your customized URL. By default, LinkedIn will provide you a profile URL that is alphanumeric and is a combination of your name and numbers.
+For example, it will look something similar to this: https://www.linkedin.com/in/venjamin-brant-73381ujy3u
+"""
+
+
+def test_linkedin_get_data():
+    linkedin_extractor = LinkedinExtractor()
+    urls = linkedin_extractor.get_data(STRING)
+    assert len(urls) == 1
+    assert "https://www.linkedin.com/in/venjamin-brant-73381ujy3u" in urls
