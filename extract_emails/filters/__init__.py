ff7393dcf85b851b399dc6b8e6914291a9a3ce58
Dmitrii Kurlov <dmitriik@tutanota.com>
2021-09-20 17:51:22 +0700

    Add IFilter to __init__

--- extract_emails/filters/__init__.py
+++ extract_emails/filters/__init__.py
@@ -1,2 +1,3 @@
-from .email_filter_interface import EmailFilterInterface
 from .default_email_filter import DefaultEmailFilter
+from .email_filter_interface import EmailFilterInterface
+from .ifilter import IFilter
