4a50dc5e2253987ea2cbf3f49f08b6e673867f3e
Dmitrii Kurlov <dmitriik@protonmail.com>
2021-10-07 17:19:37 +0700

    Fix imports for factories and default worker

--- extract_emails/__init__.py
+++ extract_emails/__init__.py
@@ -1,3 +1,8 @@
 __version__ = "5.0.1"
+from .factories import ContactFilterAndEmailAndLinkedinFactory
+from .factories import ContactFilterAndEmailFactory
+from .factories import ContactFilterAndLinkedinFactory
+from .factories import DefaultFilterAndEmailAndLinkedinFactory
 from .factories import DefaultFilterAndEmailFactory
+from .factories import DefaultFilterAndLinkedinFactory
 from .workers import DefaultWorker
--- extract_emails/factories/__init__.py
+++ extract_emails/factories/__init__.py
@@ -1,4 +1,8 @@
 from .base_factory import BaseFactory
+from .contact_filter_and_email import ContactFilterAndEmailFactory
+from .contact_filter_and_email_and_linkedin import (
+    ContactFilterAndEmailAndLinkedinFactory,
+)
 from .contact_filter_and_linkedin import ContactFilterAndLinkedinFactory
 from .default_filter_and_email import DefaultFilterAndEmailFactory
 from .default_filter_and_email_and_linkedin import (
