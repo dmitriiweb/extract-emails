7ae26d8c821c14310a9690af5e512e59ceacc206
Dmitrii Kurlov <dmitriik@protonmail.com>
2021-10-07 17:32:56 +0700

    Change version 5.0.1 -> 5.0.2

--- extract_emails/__init__.py
+++ extract_emails/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "5.0.1"
+__version__ = "5.0.2"
 from .factories import ContactFilterAndEmailAndLinkedinFactory
 from .factories import ContactFilterAndEmailFactory
 from .factories import ContactFilterAndLinkedinFactory
