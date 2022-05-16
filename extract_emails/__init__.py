13ce6593997dc9a07258737ff5294c4bda6e4034
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-05-17 00:25:47 +0700

    build: change version

--- extract_emails/__init__.py
+++ extract_emails/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "5.2.0"
+__version__ = "5.3.0"
 from .factories import ContactFilterAndEmailAndLinkedinFactory
 from .factories import ContactFilterAndEmailFactory
 from .factories import ContactFilterAndLinkedinFactory
