dd34cdeac0dbf16eaee2af1d49eb612e0bf1f830
Dmitrii K <dmitriik@protonmail.com>
2023-12-30 00:12:30 +0700

    update version and python versions

--- extract_emails/__init__.py
+++ extract_emails/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "5.3.1"
+__version__ = "5.3.2"
 from .factories import ContactFilterAndEmailAndLinkedinFactory
 from .factories import ContactFilterAndEmailFactory
 from .factories import ContactFilterAndLinkedinFactory
